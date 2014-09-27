0ad5aa846605b8ae8fe66b673b551f3e1a782592
seyren <seyrenhe@gmail.com>
2014-09-27 13:49:57 +0800

    update

--- awesome/menu.lua
+++ awesome/menu.lua
@@ -1,9 +1,10 @@
  local menu98edb85b00d9527ad5acebe451b3fae6 = {
      {"7-Zip FM", "7zFM", "/usr/share/icons/hicolor/32x32/apps/p7zip.png" },
      {"Application Finder", "xfce4-appfinder", "/usr/share/icons/gnome/16x16/actions/gtk-find.png" },
+     {"Files", "nautilus --new-window ", "/usr/share/icons/gnome/16x16/apps/system-file-manager.png" },
      {"IPython2 Qt console", "ipython2 qtconsole", "/usr/share/icons/gnome/16x16/status/gnome-netstatus-idle.png" },
-     {"Shutter", "shutter ", "/usr/share/pixmaps/shutter.png" },
-     {"Synergy", "synergy", "/usr/share/pixmaps/synergy.png" },
+     {"Shutter", "shutter ", "/usr/share/icons/hicolor/16x16/apps/shutter.png" },
+     {"Synergy", "synergy", "///usr/share/icons/synergy.ico" },
      {"Vi IMproved", "gvim -f ", "/usr/share/pixmaps/gvim.png" },
      {"gedit", "gedit ", "/usr/share/icons/gnome/16x16/apps/accessories-text-editor.png" },
      {"ipython2", "xterm -e ipython2", "/usr/share/icons/gnome/16x16/status/gnome-netstatus-idle.png" },
@@ -13,24 +14,28 @@
      {"GoldenDict", "goldendict", "///usr/share/pixmaps/goldendict.png" },
  }
 
+ local menu251bd8143891238ecedc306508e29017 = {
+     {"Fceux", "/usr/bin/fceux", "///usr/share/pixmaps/fceux.png" },
+ }
+
  local menud334dfcea59127bedfcdbe0a3ee7f494 = {
-     {"Document Viewer", "evince "},
-     {"GNU Image Manipulation Program", "gimp-2.8 "},
+     {"Document Viewer", "evince ", "/usr/share/icons/hicolor/16x16/apps/evince.png" },
+     {"GNU Image Manipulation Program", "gimp-2.8 ", "/usr/share/icons/hicolor/16x16/apps/gimp.png" },
      {"MComix", "mcomix ", "/usr/share/icons/hicolor/16x16/apps/mcomix.png" },
  }
 
  local menuc8205c7636e728d448c2774e6a4a944b = {
      {"Avahi SSH Server Browser", "/usr/bin/bssh", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
      {"Avahi VNC Server Browser", "/usr/bin/bvnc", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Chromium", "chromium ", "/usr/share/icons/hicolor/16x16/apps/chromium.png" },
      {"Dropbox", "dropboxd", "/usr/share/pixmaps/dropbox.png" },
-     {"FileZilla", "filezilla", "/usr/share/pixmaps/filezilla.png" },
+     {"FileZilla", "filezilla", "/usr/share/icons/hicolor/16x16/apps/filezilla.png" },
      {"Firefox", "/usr/lib/firefox/firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
-     {"Google Chrome", "/opt/google/chrome/google-chrome ", "/usr/share/icons/hicolor/16x16/apps/google-chrome.png" },
-     {"Skype", "skype ", "/usr/share/pixmaps/skype.png" },
+     {"Google Chrome", "/usr/bin/google-chrome-stable ", "/usr/share/icons/hicolor/16x16/apps/google-chrome.png" },
+     {"Skype", "skype ", "/usr/share/icons/hicolor/16x16/apps/skype.png" },
      {"Thunderbird", "thunderbird ", "/usr/share/icons/hicolor/16x16/apps/thunderbird.png" },
-     {"Transmission", "transmission-gtk ", "/usr/share/pixmaps/transmission.png" },
      {"Wicd", "/usr/bin/wicd-client", "/usr/share/pixmaps/wicd-gtk.xpm" },
-     {"XChat IRC", "xchat", "/usr/share/pixmaps/xchat.png" },
+     {"XChat IRC", "xchat", "/usr/share/icons/hicolor/24x24/apps/xchat.png" },
      {"Zenmap", "zenmap ", "///usr/share/zenmap/pixmaps/zenmap.png" },
      {"Zenmap (as root)", "/usr/share/zenmap/su-to-zenmap.sh ", "///usr/share/zenmap/pixmaps/zenmap.png" },
      {"aLinkCreator", "alc", "/usr/share/pixmaps/alc.xpm" },
@@ -40,35 +45,38 @@
  }
 
  local menudf814135652a5a308fea15bff37ea284 = {
-     {"Document Viewer", "evince "},
+     {"Document Viewer", "evince ", "/usr/share/icons/hicolor/16x16/apps/evince.png" },
      {"GoldenDict", "goldendict", "///usr/share/pixmaps/goldendict.png" },
      {"Kingsoft Presentation", "/usr/bin/wpp ", "/usr/share/icons/hicolor/48x48/apps/wps-office-wppmain.png" },
-     {"Kingsoft Spreadsheet", "/usr/bin/et ", "/usr/share/icons/hicolor/48x48/apps/wps-office-etmain.png" },
+     {"Kingsoft Spreadsheets", "/usr/bin/et ", "/usr/share/icons/hicolor/48x48/apps/wps-office-etmain.png" },
      {"Kingsoft Writer", "/usr/bin/wps ", "/usr/share/icons/hicolor/48x48/apps/wps-office-wpsmain.png" },
  }
 
  local menue6f43c40ab1c07cd29e4e83e4ef6bf85 = {
      {"CMake", "cmake-gui ", "/usr/share/pixmaps/CMakeSetup32.png" },
+     {"Code::Blocks IDE", "codeblocks ", "/usr/share/pixmaps/codeblocks.png" },
      {"IPython2 Qt console", "ipython2 qtconsole", "/usr/share/icons/gnome/16x16/status/gnome-netstatus-idle.png" },
-     {"Qt Assistant", "/usr/lib/qt/bin/assistant"},
-     {"Qt Creator", "/usr/bin/qtcreator"},
-     {"Qt Designer", "/usr/lib/qt/bin/designer"},
-     {"Qt Linguist", "/usr/lib/qt/bin/linguist"},
-     {"Qt QDbusViewer ", "/usr/lib/qt/bin/qdbusviewer"},
-     {"Qt4 Assistant ", "assistant-qt4"},
-     {"Qt4 Designer", "designer-qt4"},
-     {"Qt4 Linguist ", "linguist-qt4"},
-     {"Qt4 QDbusViewer ", "qdbusviewer-qt4"},
+     {"OpenJDK Policy Tool", "/usr/bin/policytool", "/usr/share/icons/hicolor/16x16/apps/java.png" },
+     {"Qt Assistant", "/usr/lib/qt/bin/assistant", "/usr/share/icons/hicolor/32x32/apps/assistant.png" },
+     {"Qt Creator", "/usr/bin/qtcreator", "/usr/share/icons/hicolor/16x16/apps/QtProject-qtcreator.png" },
+     {"Qt Designer", "/usr/lib/qt/bin/designer", "/usr/share/icons/hicolor/128x128/apps/designer.png" },
+     {"Qt Linguist", "/usr/lib/qt/bin/linguist", "/usr/share/icons/hicolor/16x16/apps/linguist.png" },
+     {"Qt QDbusViewer ", "/usr/lib/qt/bin/qdbusviewer", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer.png" },
+     {"Qt4 Assistant ", "assistant-qt4", "/usr/share/icons/hicolor/32x32/apps/assistant-qt4.png" },
+     {"Qt4 Designer", "designer-qt4", "/usr/share/icons/hicolor/128x128/apps/designer-qt4.png" },
+     {"Qt4 Linguist ", "linguist-qt4", "/usr/share/icons/hicolor/16x16/apps/linguist-qt4.png" },
+     {"Qt4 QDbusViewer ", "qdbusviewer-qt4", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer-qt4.png" },
      {"ipython2", "xterm -e ipython2", "/usr/share/icons/gnome/16x16/status/gnome-netstatus-idle.png" },
  }
 
  local menu52dd1c847264a75f400961bfb4d1c849 = {
      {"Audio Mixer", "xfce4-mixer", "/usr/share/icons/gnome/16x16/apps/multimedia-volume-control.png" },
      {"Avidemux (GTK+)", "avidemux2_gtk", "/usr/share/pixmaps/avidemux.png" },
-     {"MediaInfo", "mediainfo-gui  "},
-     {"QT V4L2 test Utility", "qv4l2"},
-     {"QjackCtl", "/usr/bin/qjackctl"},
-     {"SMPlayer", "smplayer "},
+     {"MediaInfo", "mediainfo-gui  ", "/usr/share/icons/hicolor/256x256/apps/mediainfo.png" },
+     {"PulseAudio Volume Control", "pavucontrol", "/usr/share/icons/gnome/16x16/apps/multimedia-volume-control.png" },
+     {"QjackCtl", "/usr/bin/qjackctl", "/usr/share/icons/hicolor/32x32/apps/qjackctl.png" },
+     {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
+     {"SMPlayer", "smplayer ", "/usr/share/icons/hicolor/16x16/apps/smplayer.png" },
      {"VLC media player", "/usr/bin/vlc --started-from-file ", "/usr/share/icons/hicolor/16x16/apps/vlc.png" },
      {"qt-recordMyDesktop", "qt-recordMyDesktop", "/usr/share/pixmaps/qt-recordmydesktop.png" },
      {"vokoscreen", "vokoscreen", "/usr/share/pixmaps/vokoscreen.png" },
@@ -82,12 +90,13 @@
      {"Oracle VM VirtualBox", "VirtualBox ", "/usr/share/icons/hicolor/16x16/mimetypes/virtualbox.png" },
      {"Thunar File Manager", "thunar "},
      {"Xfce Terminal", "xfce4-terminal", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
-     {"dconf Editor", "dconf-editor"},
+     {"dconf Editor", "dconf-editor", "/usr/share/icons/hicolor/16x16/apps/dconf-editor.png" },
  }
 
 xdgmenu = {
     {"Accessories", menu98edb85b00d9527ad5acebe451b3fae6},
     {"Education", menude7a22a0c94aa64ba2449e520aa20c99},
+    {"Games", menu251bd8143891238ecedc306508e29017},
     {"Graphics", menud334dfcea59127bedfcdbe0a3ee7f494},
     {"Internet", menuc8205c7636e728d448c2774e6a4a944b},
     {"Office", menudf814135652a5a308fea15bff37ea284},
